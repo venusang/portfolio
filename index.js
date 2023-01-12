12fa8c306d7cf6350dd28c92651c7a03d86dd2a0
Venus Ang <venus@users.noreply.github.com>
2023-01-11 18:48:26 -0800

    added button text swapper logic

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 let body = document.body;
-console.log("body", body);
 
+// ************************ Darkmode Button Toggle ************************ //
 let darkModeButton = document.getElementById("theme-swapper");
-console.log("darkModeButton", darkModeButton);
+
 darkModeButton.addEventListener("click", (evt) => {
   body.classList.toggle("dark-mode");
+});
+
+// ************************ Swap Text Button ***************************** //
+let counter = 0;
+let buttonText = ["one", "two", "three"];
+let swapTextButton = document.getElementById("swap-button-text");
+
+swapTextButton.addEventListener("click", (evt) => {
+  if (counter < buttonText.length) {
+    swapTextButton.textContent = `You clicked the button ${buttonText[counter]} time(s)`;
+  } else {
+    swapTextButton.textContent = `You have clicked this button more than ${buttonText.length} times`;
+  }
+  counter++;
 });
